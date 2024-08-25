f0ad44836645a7861d998e21cb6538b4fd2a82f8
powwssir <115145444+powwssir@users.noreply.github.com>
2024-08-25 15:21:33 +0800

    feat 初始化项目

---
+++ src/main/java/blog/fshare/tech/Application.java
@@ -0,0 +1,15 @@
+package blog.fshare.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: XiaoYe
+ * @create: 2024-08-25 09:35
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/blog/fshare/tech/test/ApiTest.java
@@ -0,0 +1,19 @@
+package blog.fshare.tech.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author: XiaoYe
+ * @create: 2024-08-25 09:50
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+    @Test
+    public void test() {
+        System.out.println("Code Review!");
+    }
+}
